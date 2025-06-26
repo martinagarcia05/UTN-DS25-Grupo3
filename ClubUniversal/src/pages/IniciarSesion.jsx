ebdc2f0a9df63140a6c6c7e0e161ddbd957d36c4
martinagarcia05 <marti.garcia.amendola@gmail.com>
2025-06-26 15:56:00 -0300

    addpages

---
+++ ClubUniversal/src/pages/IniciarSesion.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function IniciarSesion(){
+    return (
+        <body>
+            <h1>Iniciar Sesion</h1>
+            <form>
+                
+            </form>
+        </body>
+
+
+    );
+}
+
+
+export default IniciarSesion;
---
+++ ClubUniversal/src/pages/Registrarse.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Registrarse(){
+    return (
+        <body>
+            <h1>Registrarse</h1>
+            
+        </body>
+
+
+    );
+}
+
+
+export default Registrarse;
