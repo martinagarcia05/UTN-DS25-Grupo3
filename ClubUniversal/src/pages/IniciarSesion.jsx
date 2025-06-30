ded9ca4a885eada3ec2265b6cbd505b9af9cbe0c
martinagarcia05 <marti.garcia.amendola@gmail.com>
2025-06-30 11:14:08 -0300

    validaciones

--- ClubUniversal/src/pages/IniciarSesion.jsx
+++ ClubUniversal/src/pages/IniciarSesion.jsx
@@ -2,10 +2,104 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
+//import Nav from 'react-bootstrap/Nav';
+import { useState } from 'react';
+import Col from 'react-bootstrap/Col';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Row from 'react-bootstrap/Row';
+
 
 
 function IniciarSesion() {
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+
+  return (
+
+<Row className="justify-content-center"> {/* Centra la columna dentro de la fila */}
+      
+        <h2 className="text-center mb-4">Bienvenido!</h2>
+
+    <Form noValidate validated={validated} onSubmit={handleSubmit}>
+      <Row className="mb-3">
+
+        <Form.Group as={Col} md="4" controlId="validationCustomUsername">
+          <Form.Label>DNI</Form.Label>
+          <InputGroup hasValidation>
+            <Form.Control
+              type="text"
+              placeholder="DNI"
+              aria-describedby="inputGroupPrepend"
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Debe ingresar su DNI
+            </Form.Control.Feedback>
+            <Form.Control.Feedback>✔</Form.Control.Feedback>
+          </InputGroup>
+        </Form.Group>
+
+      </Row>
+
+    <Row className="mb-3">
+
+      <Form.Group as={Col} md="4" controlId="validationCustom02">
+          <Form.Label>Ingrese su Legajo</Form.Label>
+          <Form.Label>(en caso de no tener, no ingresar nada)</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="legajo"
+            defaultValue=""
+          />
+        </Form.Group>
+
+    </Row>
+      
+      <Row className="mb-3">  
+
+        <Form.Group as={Col} md="4" controlId="validationCustom02">
+          <Form.Label>Ingrese su Contraseña</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            placeholder="3j3mPl0"
+            defaultValue=""
+          />
+          <Form.Control.Feedback type="invalid">
+              Debe Ingresar su contraseña
+            </Form.Control.Feedback>
+          <Form.Control.Feedback>✔</Form.Control.Feedback>
+        </Form.Group>
+        
+      </Row>
+      <Form.Group className="mb-3">
+        <Form.Check
+          label="Quiero recordar sesión"
+          feedback="Ingresará a su cuenta sin necesidad de ingresar su contraseña y usuario"
+        />
+      </Form.Group>
+      <Button type="submit">Enviar</Button>
+    </Form>
+  
+  
+  </Row>
+  
+  );
+}
+
+
+export default IniciarSesion;
+
+{/*function IniciarSesion() {
    
 
 
@@ -45,6 +139,5 @@ function IniciarSesion() {
       </Form>
     </div>
   );
-}
+}*/}
 
-export default IniciarSesion
--- ClubUniversal/src/pages/Registrarse.jsx
+++ ClubUniversal/src/pages/Registrarse.jsx
@@ -1,9 +1,128 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
+//import Nav from 'react-bootstrap/Nav';
+import { useState } from 'react';
+import Col from 'react-bootstrap/Col';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Row from 'react-bootstrap/Row';
 
 function Registrarse() {
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+
+  return (
+    <Form noValidate validated={validated} onSubmit={handleSubmit}>
+      <Row className="mb-3">
+
+        <Form.Group as={Col} md="4" controlId="validationCustom01">
+          <Form.Label>Ingrese su nombre</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            placeholder="Nombre"
+            defaultValue=""
+          />
+          <Form.Control.Feedback type="invalid">
+              Debe ingresar su nombre
+            </Form.Control.Feedback>
+          <Form.Control.Feedback>✔</Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group as={Col} md="4" controlId="validationCustom02">
+          <Form.Label>Ingrese su apellido</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            placeholder="Apellido"
+            defaultValue=""
+          />
+          <Form.Control.Feedback type="invalid">
+              Debe ingresar su apellido
+            </Form.Control.Feedback>
+          <Form.Control.Feedback>✔</Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group as={Col} md="4" controlId="validationCustomUsername">
+          <Form.Label>DNI</Form.Label>
+          <InputGroup hasValidation>
+            <Form.Control
+              type="text"
+              placeholder="DNI"
+              aria-describedby="inputGroupPrepend"
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Debe ingresar su DNI
+            </Form.Control.Feedback>
+            <Form.Control.Feedback>✔</Form.Control.Feedback>
+          </InputGroup>
+        </Form.Group>
+
+      </Row>
+      <Row className="mb-3">
+
+        <Form.Group as={Col} md="4" controlId="validationCustom02">
+          <Form.Label>Ingrese su Email</Form.Label>
+          <Form.Control
+            required
+            type="email"
+            placeholder="email"
+            defaultValue=""
+          />
+          <Form.Control.Feedback type="invalid">
+              Debe ingresar su email
+            </Form.Control.Feedback>
+          <Form.Control.Feedback>✔</Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group as={Col} md="4" controlId="validationCustom02">
+          <Form.Label>Ingrese su Legajo</Form.Label>
+          <Form.Label>(en caso de no tener, no ingresar nada)</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="legajo"
+            defaultValue=""
+          />
+        </Form.Group>
+        
+        <Form.Group as={Col} md="4" controlId="validationCustom02">
+          <Form.Label>Defina su Contraseña</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            placeholder="3j3mPl0"
+            defaultValue=""
+          />
+          <Form.Control.Feedback type="invalid">
+              Debe crear su contraseña
+            </Form.Control.Feedback>
+          <Form.Control.Feedback>✔</Form.Control.Feedback>
+        </Form.Group>
+
+      </Row>
+      <Form.Group className="mb-3">
+        <Form.Check
+          label="Quiero recordar sesión"
+          feedback="Ingresará a su cuenta sin necesidad de ingresar su contraseña y usuario"
+        />
+      </Form.Group>
+      <Button type="submit">Enviar</Button>
+    </Form>
+  );
+}
+
+export default Registrarse;
+/*function Registrarse() {
   return (
     <div className="container mt-4">
         
@@ -54,6 +173,4 @@ function Registrarse() {
       </Form>
     </div>
   );
-}
-
-export default Registrarse;
+}*/
