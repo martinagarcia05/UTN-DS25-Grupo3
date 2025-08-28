7294aded84c7ee7b14f68d69c8f9191d7b4d891d
martinagarcia05 <marti.garcia.amendola@gmail.com>
2025-08-28 16:03:06 -0300

    agrego MiPerfil.js y lo linkeo al head y saludo con nombre en HomePageUser

---
+++ frontend/src/pages/MiPerfil.jsx
@@ -0,0 +1,11 @@
+function MiPerfil() {
+  /*Storage {usuario: '{"id":13,"nombre":"martina","apellido":"Garcia Ame…NINO","pais":"Argentina","email":"m1m@gmail.com"}', token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTMsI…1NTJ9.AUrjyGhsFs90QD4JBvNwpVKnNO7DecrjiOCcGaPogiM', rol: 'socio', length: 3} */
+  let nom = localStorage.getItem("nombre");
+    return (
+    <div>
+      <h1></h1>
+      
+    </div>
+  );
+}
+export default MiPerfil;
