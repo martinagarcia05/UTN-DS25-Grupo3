f8562ac6000d7af1e6cbed67432beb1fac49738d
martinagarcia05 <marti.garcia.amendola@gmail.com>
2025-08-29 15:46:58 -0300

    sociocontroller cambio

--- backend/mi-primera-api/src/controllers/SocioController.ts
+++ backend/mi-primera-api/src/controllers/SocioController.ts
@@ -17,6 +17,22 @@ export async function getSocioByDni(req: Request, res: Response) {
     res.status(500).json({ error: 'Error al buscar socio' });
   }
 }
+export async function traerUser(req: Request, res: Response) {
+const dni = Number(req.params.dni);
+
+  console.log("Buscando socio con DNI:", dni);  // <- acá
+
+  if (isNaN(dni)) return res.status(400).json({ error: 'DNI inválido' });
+
+  try {
+    const socio = await socioService.traerUser(dni);
+    if (!socio) return res.status(404).json({ error: 'Socio no encontrado' });
+    res.json(socio); // devuelve { id: 1 } si existe
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error al buscar socio' });
+  }
+}
 
 
 // let socios: Socio[] = [
