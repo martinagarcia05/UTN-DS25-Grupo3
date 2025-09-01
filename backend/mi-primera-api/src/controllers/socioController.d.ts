edde1f0229a34defa04ed9fa19761247c765e2ac
Lucia Meza <Lulimeza04@gmail.com>
2025-09-01 16:04:32 -0300

    Elimne archivos .js

--- backend/mi-primera-api/src/controllers/socioController.d.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-import { GetSocioResponse, ActualizarSocioRequest } from '../types/Socio';
-export declare function updateSocio(req: Request<{
-    id: string;
-}, GetSocioResponse, ActualizarSocioRequest>, res: Response<GetSocioResponse>, next: NextFunction): Promise<void>;
-export declare const postSocio: (req: Request, res: Response) => void;
-export declare const postValidarPswd: (req: Request, res: Response) => Response<any, Record<string, any>> | undefined;
-//# sourceMappingURL=socioController.d.ts.map
--- backend/mi-primera-api/src/types/entradas.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Socio_1 = require("./Socio");
-//# sourceMappingURL=entradas.js.map
