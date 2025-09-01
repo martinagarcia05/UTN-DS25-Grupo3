a72c0b91fc5c02fc96dae6dc149e3e434b3e6095
Lucia Meza <Lulimeza04@gmail.com>
2025-09-01 19:01:47 -0300

    Modifique las rutas

--- backend/src/routes/evento.routes.ts
+++ backend/src/routes/evento.routes.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import * as eventoController from '../controllers/eventos.controller';
 import { upload } from '../middlewares/comprobanteEntrada'
+import { createEventoSchema, updateEventoSchema } from '../validations/evento.validation';
+import { validate } from '../middlewares/validation.middleware';
 const router = Router();
 
 router.get('/', eventoController.getAllEvento);
 router.get('/:id', eventoController.getEventoById);
-router.post('/', eventoController.createEvento);
-router.put('/:id', eventoController.updateEvento);
+router.post('/', validate(createEventoSchema), eventoController.createEvento);
+router.put('/:id', validate(updateEventoSchema), eventoController.updateEvento);
 router.post('/:id/venta', upload.single("comprobante"), eventoController.registrarVenta);
 
 router.delete('/:id', eventoController.deleteEvento);
