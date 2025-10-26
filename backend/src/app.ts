4e136051c41a0736ab2552b2415a6ecef8850e49
Lucia Meza <Lulimeza04@gmail.com>
2025-10-26 17:54:38 -0300

    Cambio url

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -31,7 +31,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
-  origin: process.env.FRONTEND_URL || 'https://utn-ds-25-grupo3.vercel.app/',
+  origin: process.env.FRONTEND_URL || 'https://utn-ds-25-grupo3.vercel.app',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
